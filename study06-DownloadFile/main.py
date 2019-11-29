137c7bf0a306fa7f725511ee838ea9a14d7493ff
guiping.zhang <guiping.zhang@broadlink.com.cn>
2019-11-29 10:21:53 +0800

    add main

--- study06-DownloadFile/main.py
+++ study06-DownloadFile/main.py
@@ -17,6 +17,10 @@ headers = {
 }
 
 def get_page():
+    '''
+    获取网页中的内容，并提取文件名、下载地址
+    :return:返回[["文件名1","下载地址1"],["文件名2","下载地址2"],...]
+    '''
     url = "http://data.ess.tsinghua.edu.cn/fromglc10_2017v01.html"
     ret = requests.get(url,headers = headers)
     tmp_pattern = re.compile('</tr>(.*)</table>',re.S)
@@ -26,6 +30,12 @@ def get_page():
     return lst
 
 def download_data(filename,url):
+    '''
+    下载文件
+    :param filename: 文件名称
+    :param url: 下载地址
+    :return:
+    '''
     f_name = Fpath + filename
     try:
         ret = requests.get(url,headers)
@@ -35,19 +45,26 @@ def download_data(filename,url):
     try:
         _file = open(f_name,"wb")
         _file.write(ret.content)
+        _file.close()
     except:
         logging.error("保存失败:%s URL:%s"%(filename,url))
 
 def main():
-    if TH:
-        for i in range(len(page_data)):
+    Gidx = 0
+    if TH:  #如果是同时下载多个文件
+        for i in range(len(page_data)):  #装载线程
             th = Thread(target=download_data,args=(page_data[i][0],page_data[i][1]))
             th_lst.append(th)
-        for i in th_lst:
-            i.start()
-        for i in th_lst:
-            i.join()
-    else:
+        for idx in range(download_th):   #按组别执行线程（每组download_th个同时下载）
+            end_idx = Gidx + download_th
+            if end_idx > len(page_data):
+                end_idx = len(page_data)
+            for i in th_lst[Gidx:end_idx]:  #启动线程
+                i.start()
+            for i in th_lst[Gidx:end_idx]:  #等待线程执行结束
+                i.join()
+            Gidx += download_th             #控制当前已执行几个线程
+    else:  #单个下载
         for i in page_data:
             filename = i[0]
             url = i[1]
@@ -55,6 +72,7 @@ def main():
 
 page_data = get_page()
 Fpath = "MyDownload/"
-TH = False
+TH = False        #是否启用同时下载多个文件
+download_th = 10  #如果TH为True，则此参数表示同时下载的数据量
 th_lst = []
 main()
